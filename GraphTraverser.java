d3d54c196baa7074ec82dd8ef187ca38f47658ec
wiaan <whoppingwaki@gmail.com>
2022-11-01 11:20:46 +0200

    Depth First Traversal

--- GraphTraverser.java
+++ GraphTraverser.java
@@ -1,6 +1,25 @@
+import java.util.ArrayList;
+
 class GraphTraverser {
     
+    public static void depthFirstTraversal(Vertex start, ArrayList<Vertex> visitedVertices) {
+        System.out.println(start.getData());
+        for (Edge e: start.getEdges()) {
+            Vertex neighbour = e.getEnd();
+
+            if (!visitedVertices.contains(neighbour)) {
+                visitedVertices.add(neighbour);
+                GraphTraverser.depthFirstTraversal(neighbour, visitedVertices);
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        
+        TestGraph test = new TestGraph();
+        Vertex startingVertex = test.getStartingVertex();
+        ArrayList<Vertex> visitedVertices = new ArrayList<Vertex>();
+        visitedVertices.add(startingVertex);
+
+        GraphTraverser.depthFirstTraversal(startingVertex, visitedVertices);
     }
 }
