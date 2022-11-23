df7506ab1863bd3812b15dad943f21acfcf5a627
wiaan <whoppingwaki@gmail.com>
2022-11-23 17:50:19 +0200

    breadth first traversal

--- GraphTraverser.java
+++ GraphTraverser.java
@@ -14,12 +14,35 @@ class GraphTraverser {
         }
     }
 
+    public static void breadthFirstSearch(Vertex start, ArrayList<Vertex> visitedVertices) {
+        Queue visitedQueue = new Queue();
+        visitedQueue.enqueue(start);
+        while (!visitedQueue.isEmpty()) {
+            Vertex current = visitedQueue.dequeue();
+            System.out.println(current.getData());
+
+            for (Edge e: current.getEdges()) {
+                Vertex neighbour = e.getEnd();
+                if (!visitedVertices.contains(neighbour)) {
+                    visitedVertices.add(neighbour);
+                    visitedQueue.enqueue(neighbour);
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) {
         TestGraph test = new TestGraph();
         Vertex startingVertex = test.getStartingVertex();
-        ArrayList<Vertex> visitedVertices = new ArrayList<Vertex>();
-        visitedVertices.add(startingVertex);
 
-        GraphTraverser.depthFirstTraversal(startingVertex, visitedVertices);
+        ArrayList<Vertex> visitedVerticesDFS = new ArrayList<Vertex>();
+        visitedVerticesDFS.add(startingVertex);
+        System.out.println("Depth First Search:");
+        GraphTraverser.depthFirstTraversal(startingVertex, visitedVerticesDFS);
+
+        ArrayList<Vertex> visitedVerticesBFS = new ArrayList<Vertex>();
+        visitedVerticesBFS.add(startingVertex);
+        System.out.println("Breadth First Search:");
+        GraphTraverser.breadthFirstSearch(startingVertex, visitedVerticesBFS);
     }
 }
